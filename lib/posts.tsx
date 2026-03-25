34e51b41f9ef2574337cb88bb926a67915789d68
tuannk1 <tuannk1@hblab.vn>
2026-03-25 10:37:12 +0700

    fix types

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -6,33 +6,33 @@ import html from 'remark-html';
  
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getSortedPostsData() {
-  // Get file names under /posts
+type Post = {
+  id: string
+  title: string
+  date: string
+}
+
+export function getSortedPostsData(): Post[] {
   const fileNames = fs.readdirSync(postsDirectory);
+
   const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '');
- 
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
- 
-    // Use gray-matter to parse the post metadata section
+
     const matterResult = matter(fileContents);
- 
-    // Combine the data with the id
+
+    // 👇 ép kiểu tại đây
+    const data = matterResult.data as { title: string; date: string }
+
     return {
       id,
-      ...matterResult.data,
+      ...data,
     };
   });
-  // Sort posts by date
+
   return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
+    return a.date < b.date ? 1 : -1;
   });
 }
 
