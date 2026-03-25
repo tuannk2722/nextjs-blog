fb92c5caae4fc157c543ac8db875c98d7a49123c
tuannk1 <tuannk1@hblab.vn>
2026-03-25 10:17:31 +0700

    fix function GET dynamic api

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,7 +1,13 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: NextApiRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
-    return NextResponse.json({ userId: id, message: `Hello user ${id}` });
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params
+
+  return NextResponse.json({
+    userId: id,
+    message: `Hello user ${id}`
+  })
 }
