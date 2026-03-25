cb5c045cb0ac47bf447c376a0c60477422513d2e
tuannk1 <tuannk1@hblab.vn>
2026-03-25 10:27:34 +0700

    fix function GET dynamic api route

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -2,12 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params
+  const { id } = await context.params
 
   return NextResponse.json({
     userId: id,
-    message: `Hello user ${id}`
+    message: `Hello user ${id}`,
   })
 }
